01595100c50bb1cab486f2173e7982c20da605b5
Anurag Gaur <anuraggaur29@gmail.com>
2026-05-09 08:47:37 +0530

    Updated bg

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,20 @@ import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
+import path from "node:path";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://aish999.netlify.app',
+  site: 'https://anuraggaur29.netlify.app',
   integrations: [sitemap(), tailwind(), react()],
   vite: {
+    resolve: {
+      alias: {
+        "@": path.resolve("./src"),
+        "@components": path.resolve("./src/components"),
+        "@layouts": path.resolve("./src/layouts"),
+      },
+    },
     build: {
       sourcemap: false
     }
