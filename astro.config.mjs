74d9acb3bf4484df6ad9318954d3ed97c526dbb8
Anurag Gaur <anuraggaur29@gmail.com>
2026-05-09 09:03:04 +0530

    Revert background theme update

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,20 +2,12 @@ import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
-import path from "node:path";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://anuraggaur29.netlify.app',
   integrations: [sitemap(), tailwind(), react()],
   vite: {
-    resolve: {
-      alias: {
-        "@": path.resolve("./src"),
-        "@components": path.resolve("./src/components"),
-        "@layouts": path.resolve("./src/layouts"),
-      },
-    },
     build: {
       sourcemap: false
     }
